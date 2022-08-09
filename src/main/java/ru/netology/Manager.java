d31e4b8c42171c9e5bd0e28f0b4389cff9e04202
Pankeeva Olga <0lga.pankeeva@mail.ru>
2022-08-09 14:13:29 +0400

    commit init

---
+++ src/main/java/ru/netology/Manager.java
@@ -0,0 +1,46 @@
+package ru.netology;
+
+public class Manager {
+
+    private int filmLimit = 10;
+    private String[] film = new String[0];
+
+    public void addFilm(String films) {
+        String[] tmp = new String[film.length + 1];
+        for (int i = 0; i < film.length; i++) {
+            tmp[i] = film[i];
+        }
+        tmp[tmp.length - 1] = films;
+        film = tmp;
+    }
+
+
+    public Manager() {
+    }
+
+    public Manager(int filmLimit) {
+        this.filmLimit = filmLimit;
+
+    }
+
+    public String[] findAll() {
+        return film;
+    }
+
+    public String[] findLast() {
+        int result;
+        if (film.length < filmLimit) {
+            result = film.length;
+        } else {
+            result = filmLimit;
+        }
+        String[] List = new String[result];
+        for (int i = 0; i < List.length; i++) {
+            List[i] = film[film.length - i - 1];
+        }
+        return List;
+    }
+}
+
+
+
